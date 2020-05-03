0e5431de9c5f796b40d8c305d0b33b6da2d7a58b
carlosjosetri <00082217@uca.edu.sv>
2020-05-03 17:47:45 -0600

    primera parte

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.uca.capas.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.uca.capas.domain.Student;
+
+@Controller
+public class MainController {
+  
+	@GetMapping(path = "/ejemplo1" , produces= MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String ejemplo1() {
+		return "bienvenido\n"+"programacion N Capas";
+    	
+    }
+	@GetMapping("/ejemplo2")
+
+    public @ResponseBody List<Student> ejemplo2() {
+		return Arrays.asList(
+	(new Student("Nombre1","Apellido1","carrera1","10/10/2020",true)),
+	(new Student("Nombre2","Apellido2","carrera2","10/10/2020",false))
+				)
+				;
+    	
+    }
+}
